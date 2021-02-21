84d47ec0387f29ed7dbf98edfb2aeb364250143f
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2021-02-21 18:53:56 +0900

    change

--- db/schema.rb
+++ db/schema.rb
@@ -58,8 +58,8 @@ ActiveRecord::Schema.define(version: 2021_02_07_111603) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "nickname", null: false
     t.string "familyname", null: false
     t.string "firstname", null: false
