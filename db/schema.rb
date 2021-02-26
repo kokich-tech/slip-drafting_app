3f74f533f5ef12476652b08a7d216a17733b87c0
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2021-02-26 22:47:18 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2021_02_07_111603) do
     t.datetime "remember_created_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "withdrawals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
